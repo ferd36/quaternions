96789470911d02286871b36ad5fd0cc2c6c5f57e
Frank Astier <fastier@linkedin.com>
2015-12-16 09:27:59 -0800

    Added Makefile.mk separate from CMake

---
+++ Makefile.mk
@@ -0,0 +1,20 @@
+# Makefile outside of cmake
+
+BOOST_INCLUDE_DIR = /usr/local/include
+BOOST_LIB_DIR     = /usr/local/lib
+CXX               = g++
+CXX_FLAGS         = -std=c++11 -Wall -DNDEBUG -O3
+DEPS              = src/Quaternion.h
+OBJS              = main.o
+LIBS              = -lstdc++
+
+all: quaternions
+
+quaternions: $(OBJS)
+	$(CXX) -o $@ $^ $(CXX_FLAGS) -L$(BOOST_LIB_DIR) $(LIBS)
+
+%.o: src/%.cpp $(DEPS)
+	$(CXX) $(CXX_FLAGS) -c -o $@ $<
+
+clean:
+	rm *.o quaternions
