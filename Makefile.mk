3686c9ce2c3848829b5e39cdfbc6c82012d45986
FrankAstier <fastier@yahoo.com>
2015-12-22 15:07:09 -0800

    coveralls in travis

--- Makefile.mk
+++ Makefile.mk
@@ -1,7 +1,7 @@
 # Makefile outside of cmake
 
 #CXX               = g++
-CXX_FLAGS         = -std=c++11 -Wall -DNDEBUG -O3
+CXX_FLAGS         = -std=c++11 -Wall -DNDEBUG -O3 --coverage
 DEPS              = src/quaternion.h
 OBJS              = unit_tests.o
 
@@ -9,6 +9,7 @@ all: unit_tests
 
 unit_tests: $(OBJS)
 	$(CXX) -o $@ $^ $(CXX_FLAGS)
+	lcov --capture --directory . --output-file coverage.info
 
 %.o: src/%.cpp $(DEPS)
 	$(CXX) $(CXX_FLAGS) -c -o $@ $<
