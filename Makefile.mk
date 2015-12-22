51f2d95e93000b9de567ab88f633b779d7813b10
FrankAstier <fastier@yahoo.com>
2015-12-22 15:19:20 -0800

    Travis gcc build

--- Makefile.mk
+++ Makefile.mk
@@ -1,7 +1,6 @@
 # Makefile outside of cmake
 
-#CXX               = g++
-CXX_FLAGS         = -std=c++11 -Wall -DNDEBUG -O3 --coverage
+CXX_FLAGS         = -std=c++11 -Wall -DNDEBUG -O3
 DEPS              = src/quaternion.h
 OBJS              = unit_tests.o
 
@@ -9,7 +8,6 @@ all: unit_tests
 
 unit_tests: $(OBJS)
 	$(CXX) -o $@ $^ $(CXX_FLAGS)
-	lcov --capture --directory . --output-file coverage.info
 
 %.o: src/%.cpp $(DEPS)
 	$(CXX) $(CXX_FLAGS) -c -o $@ $<
